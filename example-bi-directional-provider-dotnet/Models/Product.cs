23981c63b46e9abae06a4c480a67878f9a95d1da
Frank Kilcommins <frank.kilcommins@smartbear.com>
2022-09-02 22:49:22 +0100

    breaking change?

--- example-bi-directional-provider-dotnet/Models/Product.cs
+++ example-bi-directional-provider-dotnet/Models/Product.cs
@@ -1,9 +1,12 @@
-﻿namespace Provider.Models
+﻿using System;
+
+namespace Provider.Models
 {
     public class Product
     {
-        public int id { get; set; }
+        public Guid id { get; set; }
         public string type { get; set; }
         public string name { get; set; }
     }
+
 }
